370d9d1dca9c28d3c8de207fcaf20e6000cda75e
Mahyar <mahyar.erfanian1998@gmail.com>
2024-05-31 12:17:47 -0400

    Add middleware for production

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
 const cors = require("cors");
+const createError = require("http-errors");
 require("dotenv").config();
 
 // Require the routers
@@ -25,10 +29,31 @@ app.use(express.json());
 // Use morgan as HTTP logger to show the HTTP method and route of each request
 app.use(morgan("dev"));
 
-app.use(cors());
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: { "script-src": ["'self'"] },
+  }),
+);
+const limiter = RateLimit({
+  windowMS: 1 * 60 * 1000,
+  max: 40,
+});
+app.use(limiter);
+
+// Enable CORS
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    maxAge: 600,
+  }),
+);
 
 // Use the routers
-app.get("/", (req, res) => res.send("hi"));
+app.get("/", (req, res) => res.send("Welcome to LyricsLexis backend API"));
 app.use("/", authRouter);
 app.use("/users", usersRouter);
 app.use("/users", songsRouter);
@@ -47,6 +72,19 @@ app.get("/api/dictionary/:word", async (req, res) => {
     console.error("Error fetching data:", error);
     res.status(500).json({ error: "An error occurred while fetching data." });
   }
+});
+
+// Use http-errors middleware to generate a 404 error in case no route matches
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Use a custom error handler middleware
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.json({ error: err.message });
 });
 
 const port = process.env.PORT || 3000;
